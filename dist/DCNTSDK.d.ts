102fd0034677ad4029fda16b663508c293fb0e63
Sweets Sweetman <sweetmantech@gmail.com>
2022-09-14 17:51:17 -0300

    I update dist

--- dist/DCNTSDK.d.ts
+++
@@ -1,4 +0,0 @@
-import { ethers, Signer } from "ethers";
-import { Provider } from "@ethersproject/providers";
-import { Chain } from './chains';
-export declare const setupDCNTSDK: (chainOrAddress: Chain | string, signerOrProvider: Signer | Provider) => Promise<ethers.Contract>;
