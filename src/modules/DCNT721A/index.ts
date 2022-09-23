6c7dcb7e60d0a254383aa135af4d154094719be6
zev <zev@decent.xyz>
2022-09-22 20:40:34 -0400

    update 721a

--- src/modules/DCNT721A/index.ts
+++ src/modules/DCNT721A/index.ts
@@ -1,6 +1,7 @@
 import { SDK } from "../../sdk";
 import { ethers, BigNumber, Contract } from "ethers";
 import DCNT721A from './contracts/DCNT721A.json';
+import { MetadataRendererInit } from '../DCNTMetadataRenderer';
 
 export const deployDCNT721A = async (
   sdk: SDK,
@@ -8,14 +9,35 @@ export const deployDCNT721A = async (
   symbol: string,
   maxTokens: number,
   tokenPrice: BigNumber,
-  maxTokenPurchase: number
+  maxTokenPurchase: number,
+  royaltyBPS: number,
+  metadataURI: string,
+  metadataRendererInit: MetadataRendererInit | null
 ) => {
+  const encodedMetadata = metadataRendererInit != null
+    ? ethers.utils.AbiCoder.prototype.encode(
+        ['string', 'string', 'string'],
+        [
+          metadataRendererInit.description,
+          metadataRendererInit.imageURI,
+          metadataRendererInit.animationURI
+        ]
+      )
+    : [];
+
   const deployTx = await sdk.contract.deployDCNT721A(
-    name,
-    symbol,
-    maxTokens,
-    tokenPrice,
-    maxTokenPurchase
+    {
+      name,
+      symbol,
+      maxTokens,
+      tokenPrice,
+      maxTokenPurchase,
+      royaltyBPS,
+    },
+    {
+      metadataURI,
+      metadataRendererInit: encodedMetadata,
+    }
   );
 
   const receipt = await deployTx.wait();
@@ -34,4 +56,3 @@ export const getDCNT721A = async (
     sdk.signerOrProvider
   );
 }
-
